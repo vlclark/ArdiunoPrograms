2501e5cc91b0838b403cb4a97a698e348d0c6384
Vance Clark <02vance.clark@gulllakecs.org>
2022-02-24 09:09:17 -0800

    Single Blink - Using PinMode 9 on board

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -22,16 +22,19 @@
   https://www.arduino.cc/en/Tutorial/BuiltInExamples/Blink
 */
 
+// Programmer: Vance Clark
+// Date: 2.22.2022
+
 // the setup function runs once when you press reset or power the board
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  // initialize digital pin 9 as an output.
+  pinMode(9, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(9, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(9, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);                       // wait for a second
 }
